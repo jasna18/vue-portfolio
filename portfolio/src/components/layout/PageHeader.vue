8fb6f0b94e817ba3498e521d95f154c712fc5119
jasna18 <jasnamnaz@gmail.com>
2025-10-24 19:38:38 +0400

    added links with router

--- portfolio/src/components/layout/PageHeader.vue
+++ portfolio/src/components/layout/PageHeader.vue
@@ -3,20 +3,30 @@
    :class="darkMode ? 'bg-gray-900 text-white' : 'text-black'" class="transition-colors duration-500">
   <!-- Navbar -->
 <header :style="{ backgroundColor: darkMode ? '#111827' : '#A7C1A8' }" class="fixed top-0 w-full z-10">
-  <nav class="flex justify-between items-center px-6 py-3">
+  <nav class="flex justify-between items-center px-4 py-3">
     <!-- Mobile menu button -->
     <button class="text-2xl md:hidden" @click="toggleMenu">☰</button>
 
     <!-- Navbar links -->
-    <ul
+    <ul class="font-bold"
       :class="[
         menuOpen ? 'flex' : 'hidden',
         'md:flex flex-col md:flex-row gap-4 md:gap-8 absolute md:static top-14 p-4 md:p-0 shadow md:shadow-none ml-auto mr-100'
       ]">
-      <li><a href="#hero" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">Home</a></li>
+    <router-link
+            v-for="link in links"
+            :key="link.path"
+            :to="link.path"
+            :class="{ 'active-link': $route.path === link.path }"
+            @click="closeMenu"
+          >
+            {{ link.name }}
+  </router-link>
+
+      <!-- <li><a href="#hero" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">Home</a></li>
       <li><a href="#about" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">About</a></li>
       <li><a href="#projects" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">Projects</a></li>
-      <li><a href="#contact" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">Contact</a></li>
+      <li><a href="#contact" @click="closeMenu" class="font-bold" :class="darkMode ? 'text-white' : 'text-black'">Contact</a></li> -->
     </ul>
 
     <!-- Dark Mode Toggle -->
@@ -31,4 +41,13 @@
 <script setup>
 import { menuOpen, toggleMenu, closeMenu, darkMode, toggleDark } from '../../data/darkMode.js';
 import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+const links = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Projects", path: "/projects" },
+  { name: "Contact", path: "/contact" },
+];
 </script>
