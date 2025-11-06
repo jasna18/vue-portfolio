ef4abd93ad871085e4c4a33af48fa19a56403859
jasna18 <jasnamnaz@gmail.com>
2025-11-06 20:46:41 +0400

    fixed responsiveness on div component md

--- portfolio/src/composobles/DivComponent.vue
+++ portfolio/src/composobles/DivComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hidden sm:inline-block md:inline-block lg:inline-block text-xs md:text-sm lg:text-sm tracking-wide rounded-full 
-               dark:bg-gray-800/40 border border-white/10 backdrop-blur-md shadow-lg p-1
+               dark:bg-gray-800/40 border border-white/10 backdrop-blur-md shadow-lg 
                 bg-white/30 text-[#000000] md:mb-6 transition duration-300 ease-in-out 
                  hover:bg-[#cfafa6] hover:shadow-lg inset-shadow-sm shadow-orange-500/50" :class="[width]" >
                   {{ text }}             
