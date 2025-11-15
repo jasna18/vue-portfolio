5eedb4266861164ea3c705ceceb0ea69269ab0f7
jasna18 <jasnamnaz@gmail.com>
2025-11-15 14:00:33 +0400

    Fix blank page: Use relative base path

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-    base: '/vue-portfolio/',
+    base: 'vue-portfolio/',
 
 })
